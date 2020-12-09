1c8ac55344f19f65230007d7db8879267f7aa1de
Sergio Ribeiro <ssribeiro@gmail.com>
2020-12-09 00:48:08 -0300

    Example1

---
+++ example1.py
@@ -0,0 +1,21 @@
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    
+    subject = 'Grav dinner this weekend'
+    body = 'How about dinner at 6pm this saturday?'
+    
+    msg = f'Subject:{subject} \n\n {body}
+    print(msg)
+    smtp.sendmail(EMAIL_ADDRESS, 'sergio@lmmp.mec.puc-rio.br')
