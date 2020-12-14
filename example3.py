79e6b4196ba6f5534c54abeba12aa4368b8b29d6
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2020-12-14 16:35:54 -0300

    OrderClientData

--- example3.py
+++ example3.py
@@ -20,4 +20,30 @@ orderDate = datetime.strptime(orderDateText,'%d/%m/%Y %H:%M')
 
 
 # Get all order info together
-match = soup.find_all('td padding:="" solid="" style='3D"border-bottom:' valign='3D"middle=')
+match = soup.find_all('s')
+
+# Client Name
+orderClientName =match[1].text
+
+# Client CPF
+orderClientCPF =match[3].text
+
+# Client Address
+orderClientAddress =match[4].text
+
+# Get all order info together
+match = soup.find_all('span')
+
+# Order Origin
+orderOrigin = match[2].text[18:]
+
+# Client Email
+orderClientEmail = match[5].text.replace('=\n    ','')
+
+# Client Phone
+orderClientPhone = match[7].text.replace('=\n    ','')
+
+# Client Mobile
+orderClientMobile = match[9].text.replace('=\n    ','')
+
+# TODO: Order Details
