40e88ea825049e63d5b6398948500392e0e60900
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2021-01-13 08:48:18 -0300

    DataDictionaries

--- example3.py
+++ example3.py
@@ -32,8 +32,48 @@ orderClientCPF =match[7].text
 # Client Address
 orderClientAddress = match[9].text.replace('\n        ',' - ').replace('\n',' - ')
 
-# TODO: Order Details
+# Client Data Dictionary
+orderClientData = {'CPF': orderClientCPF,
+                   'Name': orderClientName,
+                   'Email': orderClientEmail,
+                   'Mobile': orderClientMobile,
+                   'Address': orderClientAddress}
+# Order Shipment Type
+orderChipment = 
+
+# Order Items Initialization
+orderItems = [{}]
+
+# Match Data
 matchDiv = soup.find('div',style="color: #475C7A; padding: 5px 20px 10px; border: 5px solid #EFF8FF;")
 matchTable = matchDiv.find_all('table',border="0")
+tableLines = matchTable[2].find_all('tr')
+
+orderItems = []
+orderSubtotal = 0
+
+# Append Column Values
+for line in tableLines[1:]:
+    columns = line.find_all('td')
+    ItemId = columns[1].text[columns[1].text.find('SKU:')+5:-2]
+    ItemDescrition = columns[1].text.replace('  ','').replace('\n',' ')
+    ItemQty = int(columns[2].text)
+    ItemPrice = float(columns[3].text.replace('R$ ',''))
+    orderItems.append({'Id':ItemId ,
+                       'Description':ItemDescrition,
+                       'Quantity':ItemQty,
+                       'Price':ItemPrice})
+    orderSubtotal = orderSubtotal+ItemPrice
+
+# Order Shipment Type
+tableLinesNew = matchTable[3].find_all('tr')
+OrderShipment = tableLinesNew[1].find_all('td').text
 
 
+# Order Complete Data
+orderData = {'Number': orderNumber,
+             'DateTime': mktime(datetime.now().timetuple()),
+             'Client': orderClientData['CPF'],
+             'Shipment': OrderShipment,
+             'Subtotal': orderSubtotal
+             'Items': orderItems}
