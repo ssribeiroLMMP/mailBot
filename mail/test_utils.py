145838de76e73743e7d92a144de2e772fdefdc58
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2021-01-17 07:21:03 -0300

    pkgRenaming

---
+++ mail/test_utils.py
@@ -0,0 +1,29 @@
+#!./env/bin/python3
+import unittest
+import utils as ut
+import time
+from dotenv import load_dotenv 
+
+class TestUtils(unittest.TestCase):
+
+    # Test class population
+    def test_loadDotEnv(self):
+        load_dotenv()
+        emailServer = ut.EmailServer()
+        self.assertIsNotNone(emailServer.SMTP_PORT,msg='Is None!')
+
+    def test_sendMail(self):
+        emailServer = ut.EmailServer()
+        recipients = {'To':'sergio@lmmp.mec.puc-rio.br',
+            'Cc':'ssribeiro@gmail.com'}
+
+        subject = 'New Test Message'
+        body = 'How about dinner at 6pm this saturday?'
+        
+        # Test
+        result,exception = emailServer.sendMessage(recipients,subject,body)
+        self.assertTrue(result)
+
+# Attribute main call to mailbot_app
+if __name__ == '__main__':
+    unittest.main()
