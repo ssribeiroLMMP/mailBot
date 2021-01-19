5d360f58e04219f1f55bb3501026db2605290d64
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2021-01-19 11:40:08 -0300

    UnitTests_CheckSendReceive

--- mail/test_utils.py
+++ mail/test_utils.py
@@ -2,28 +2,54 @@
 import unittest
 import utils as ut
 import time
-from dotenv import load_dotenv 
 
 class TestUtils(unittest.TestCase):
 
     # Test class population
     def test_loadDotEnv(self):
-        load_dotenv()
         emailServer = ut.EmailServer()
-        self.assertIsNotNone(emailServer.SMTP_PORT,msg='Is None!')
+        return self.assertIsNotNone(emailServer.SMTP_PORT,msg='Is None!')
 
-    def test_sendMail(self):
+    # Test send message method
+    def test_sendMessage(self):
         emailServer = ut.EmailServer()
-        recipients = {'To':'sergio@lmmp.mec.puc-rio.br',
+        recipients = {'To':'sergio@lmmp.mec.puc-rio.br', # 'lojavirtual@stelio.com.br',# 
             'Cc':'ssribeiro@gmail.com'}
 
         subject = 'New Test Message'
         body = 'How about dinner at 6pm this saturday?'
         
-        # Test
+        # Check if Result is True
         result,exception = emailServer.sendMessage(recipients,subject,body)
-        self.assertTrue(result)
+        return self.assertTrue(result)
+
+    # Test Check for Mails method
+    def test_checkForMails(self):
+        mailbox='INBOX'
+        filterQuery='UNSEEN'
+        #Check for Mails
+        emailServer = ut.EmailServer()
+        imap,mailIDs,numOfMails = emailServer.checkForMails(mailbox,filterQuery)
+        
+        # Chech if it returns a number greater than zero
+        return self.assertGreater(numOfMails,0)
+
+    # Test Check for Mails method
+    def test_readMessages(self):
+        mailbox='INBOX'
+        filterQuery='UNSEEN'
+        #Check for Mails
+        emailServer = ut.EmailServer()
+        messagesList = emailServer.readMessages(mailbox='INBOX',filterQuery='UNSEEN',htmlFile = None)
+        
+        # Check if it returns a list
+        return self.assertIsInstance(messagesList,type([1,2]))
+    
+def main():
+    testUtils = TestUtils()
+    testUtils.test_readMessages()
 
 # Attribute main call to mailbot_app
 if __name__ == '__main__':
     unittest.main()
+    # main()
--- mail/utils.py
+++ mail/utils.py
@@ -1,17 +1,123 @@
 import os
-import smtplib
+import smtplib, imaplib, email
+from bs4 import BeautifulSoup
 from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from dotenv import load_dotenv 
+from datetime import datetime
+# from imap_tools import Q, AND, OR, NOT
 
 
+# def buildFilterQuery(fieldsDict):
+    
+#     filterQuery = 
+    
+#     return filterQuery
+
+# Email Server Class
 class EmailServer():
+
     def __init__(self,dotEnv_path=None):
+        load_dotenv()
         # load_dotenv(dotEnv_path)
         self.SMTP_SERVER = os.environ.get('SMTP_SERVER')
+        self.IMAP_SERVER = os.environ.get('IMAP_SERVER')
         self.SMTP_PORT = os.environ.get('SMTP_PORT')
         self.EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
         self.EMAIL_PASSWORD = os.environ.get('APP_PASS')
+        self.TIME_FORMAT = os.environ.get('TIME_FORMAT')
+        print(self.SMTP_PORT)
+        print(self.IMAP_SERVER)
+
+    # Method: Check for mails in certain mailbox and filter query
+    def checkForMails(self,mailbox='INBOX',filterQuery='UNSEEN'):
+        # Context
+        with imaplib.IMAP4_SSL(self.IMAP_SERVER) as imap:
+            # Login
+            typ, _ =imap.login(self.EMAIL_ADDRESS,self.EMAIL_PASSWORD)
+            print(typ)
+            # Select Mailbox
+            imap.select(mailbox)
+
+            #Search on specific folder/marker
+            _,search_data, = imap.search(None,filterQuery)
+            
+            mailIDs = search_data[0].split()
+            print(len(mailIDs))
+            return imap, mailIDs, len(mailIDs)
+
+    def readMessages(self,mailbox='INBOX',filterQuery='UNSEEN',htmlFile = None):
+
+        # #Check for Mails
+        # imap,mailIDs,numOfMails = self.checkForMails(mailbox,filterQuery)
+
+        # Context
+        with  imaplib.IMAP4_SSL(self.IMAP_SERVER) as imap:
+            # Login
+            typ, _ =imap.login(self.EMAIL_ADDRESS,self.EMAIL_PASSWORD)   
+            messagesList = []
+            # Select Mailbox
+            imap.select(mailbox)
+
+            #Search on specific folder/marker
+            _,search_data, = imap.search(None,filterQuery)
+            # Split Mail IDs
+            mailIDs = search_data[0].split()
+            
+            # Loop over search results
+            for mailID in mailIDs:
+                messageDict = {}
+                # Get mail data
+                _,mailData = imap.fetch(mailID,'(RFC822)')
+                # Collect message from mail data
+                _,mail_bytes = mailData[0]
+                # Scrape mail with BeatifulSoup Library
+                soup = BeautifulSoup(mail_bytes, 'lxml')
+                # print(soup.prettify())
+                
+                # If desired write html file with mail content
+                if (htmlFile != None):
+                        # File Context
+                    with open(htmlFile, "w") as file:
+                        # write message into html File
+                        file.write(str(soup))
+                
+
+                emailMessage = email.message_from_bytes(mail_bytes)
+                
+                # Read Mail Header
+                email_header = {}
+                for headerTag in ['subject','to','from','date']:
+                    
+                    if headerTag == 'date':
+                        dateTimeData = datetime.strptime(emailMessage[headerTag],self.TIME_FORMAT)
+                        email_header[headerTag] = dateTimeData.timestamp()
+                        # email_header['Timezone'] = dateTimeData.timetz().tzname()
+                    else:
+                        email_header[headerTag] = emailMessage[headerTag]
+
+                    print("{}:{}".format(headerTag,email_header[headerTag])) 
+                                    
+                # Sets Dict Header
+                messageDict['header'] = email_header
+                
+                # Read Mail Body
+                for part in emailMessage.walk():
+                    if part.get_content_type()=="text/plain":
+                        email_body = part.get_payload(decode=True).decode()
+
+                    elif part.get_content_type()=="text/html":
+                        email_body = part.get_payload(decode=True).decode()
+
+                # Sets Dict Body 
+                messageDict['body'] = email_body
+                print(messageDict)
+                #Append to messages list
+                messagesList.append(messageDict)
+
+        return messagesList    
+
 
     def sendMessage(self,recipients,subject,body):
         
@@ -29,6 +135,7 @@ class EmailServer():
             msg['Subject'] = subject
             msg['From'] = self.EMAIL_ADDRESS
             msg['To'] = recipients['To']
+            msg['Cc'] = recipients['Cc']
             msg.attach(MIMEText(body, 'plain'))
             
             # Send Mail
